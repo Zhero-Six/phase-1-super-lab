ce2fd86f9d27736df392ab1d9a8f2c3d96826779
Zhero-Six <vmochoge@gmail.com>
2025-04-02 20:18:21 +0300

    solves super

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your classes here
+// index.js
+
+class Tree {
+    constructor(species) {
+        this.species = species;
+    }
+
+    static definition() {
+        return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+    }
+}
+
+class Deciduous extends Tree {
+    constructor(species, name) {
+        super(species);  // Call parent constructor with species
+        this.name = name;
+    }
+
+    static definition() {
+        return `${super.definition()} Deciduous trees shed their leaves annually.`;
+    }
+}
+
+class Evergreen extends Tree {
+    constructor(species, name) {
+        super(species);  // Call parent constructor with species
+        this.name = name;
+    }
+
+    static definition() {
+        return `${super.definition()} Evergreens keep their leaves all year round.`;
+    }
+}
+
+// Export for testing
+if (typeof module !== 'undefined') {
+    module.exports = { Tree, Deciduous, Evergreen };
+}
